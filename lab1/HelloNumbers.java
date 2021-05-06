78f5042edb44f800920c4e11fc33092b97faf272
Squirrel <389214993@qq.com>
2021-05-06 16:32:14 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,15 @@
+/** Exercise 1.1.2
+ *  @author zhengqijun
+ */
+
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum = 0;
+        while (x < 10) {
+            sum += x;
+            x++;
+            System.out.println(sum);
+        }
+    }
+}
